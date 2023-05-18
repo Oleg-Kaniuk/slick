4af7c0bff8185551f96ccd37f185799f7d326e50
Oleg-Kaniuk <129388390+Oleg-Kaniuk@users.noreply.github.com>
2023-05-18 17:39:45 +0300

    added scripts

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,31 @@
+// $('.single-item').slick({
+//   dots: true,
+//   dotsClass: "my-dots",
+//   autoplay: true,
+//   autoplaySpeed: 2000,
+// }
+// );
+
+
+// $('.autoplay').slick({
+//     dots: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 1,
+//     dotsClass: "my-dots",
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+// });
+
+
 $('.multiple-items').slick({
-  infinite: true,
+    infinite: true,
+     dots: true,
+    dotsClass: "my-dots",
   slidesToShow: 3,
   slidesToScroll: 3
 });
-			
+
+
+
+
+		
